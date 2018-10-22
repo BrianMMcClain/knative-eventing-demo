0e3ecbf2818d10ec929f4151610a567782c1e6bf
Brian McClain <brianmmcclain@gmail.com>
2018-10-22 16:46:39 -0400

    Initial commit

---
+++ app.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func handlePost(rw http.ResponseWriter, req *http.Request) {
+	defer req.Body.Close()
+	body, _ := ioutil.ReadAll(req.Body)
+	fmt.Fprintf(rw, "Request Body: %s", body)
+	log.Printf("Request: %s", body)
+}
+
+func main() {
+	log.Print("Starting server on port 8080...")
+	http.HandleFunc("/", handlePost)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
